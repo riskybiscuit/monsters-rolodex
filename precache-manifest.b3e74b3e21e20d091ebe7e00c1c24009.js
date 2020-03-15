1eee2059e9b0a404494fa746e2fded0b5f584a1e
Lilian N <izzopod@gmail.com>
2020-03-15 18:22:58 -0500

    Updates

---
+++ precache-manifest.b3e74b3e21e20d091ebe7e00c1c24009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597e491fff44fb1eb0b3ceab70689a8a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "82d066ac1ce5ccebe417",
+    "url": "/monsters-rolodex/static/css/main.271ed155.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d066ac1ce5ccebe417",
+    "url": "/monsters-rolodex/static/js/main.c53467c2.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
